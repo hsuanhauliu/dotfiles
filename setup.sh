8ca629c27a5119d685f6010a7366c7594bd1a813
Hsuan-Hau Liu <23202168+hsuanhauliu@users.noreply.github.com>
2022-03-30 00:30:28 -0700

    chore: improve setup script, tmux config

--- setup.sh
+++ setup.sh
@@ -10,31 +10,31 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # Install plugins
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+git clone --depth 1 https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 ## Tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ## VIM
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/jez/vim-superman ~/.vim/bundle/vim-superman
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
-git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
-git clone https://github.com/tpope/vim-surround.git  ~/.vim/bundle/vim-surround
-git clone https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown
-git clone https://github.com/sirver/ultisnips.git ~/.vim/bundle/ultisnips
-git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
-git clone https://github.com/easymotion/vim-easymotion.git ~/.vim/bundle/vim-easymotion
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
-git clone https://github.com/klen/python-mode.git ~/.vim/bundle/python-mode
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone --depth 1 https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
+git clone --depth 1 https://github.com/jez/vim-superman ~/.vim/bundle/vim-superman
+git clone --depth 1 https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+git clone --depth 1 https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+git clone --depth 1 https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
+git clone --depth 1 https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+git clone --depth 1 https://github.com/tpope/vim-surround.git  ~/.vim/bundle/vim-surround
+git clone --depth 1 https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown
+git clone --depth 1 https://github.com/sirver/ultisnips.git ~/.vim/bundle/ultisnips
+git clone --depth 1 https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone --depth 1 https://github.com/easymotion/vim-easymotion.git ~/.vim/bundle/vim-easymotion
+git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone --depth 1 https://github.com/klen/python-mode.git ~/.vim/bundle/python-mode
 cd ~/.vim/bundle/python-mode
 git submodule update --init --recursive
 cd -
 
 ## ZSH
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
